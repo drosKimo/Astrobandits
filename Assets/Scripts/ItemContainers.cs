e38e6f7308d7fdd35f53c6dbb6238b50e620eac0
drosKimo <ilikeuflower@yandex.ru>
2024-07-21 17:35:53 +0300

    Cards, scaling and TMPro

---
+++ Assets/Scripts/ItemContainers.cs
@@ -0,0 +1,56 @@
+using Akassets.SmoothGridLayout;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemContainers : MonoBehaviour
+{
+    int defaultItems = 5, // минимальное число предметов до уменьшения расстояния
+        spacingItems; // количество объектов
+
+    Vector2 spacingOffset, // какое значение мы устанавливаем
+            spacingDefault; // базовое значение
+
+    GridLayoutGroup gridLayoutGroup;
+
+    void Awake()
+    {
+        // получаем компонент сетки, чтобы изменять расстояние
+        gridLayoutGroup = gameObject.GetComponent<GridLayoutGroup>();
+
+        spacingDefault = gridLayoutGroup.spacing;
+    }
+
+    void LateUpdate()
+    {
+        spacingItems = gameObject.transform.childCount; // количество дочерних объектов
+
+        // примерно на 18 карте этот способ начинает плохо работать, но до такого количества карт не дойдет (скорее всего)
+        if (spacingItems > defaultItems)
+        {
+            int restItems = spacingItems - defaultItems;
+            spacingOffset.x = spacingDefault.x - (10 * restItems);
+
+            gridLayoutGroup.spacing = new Vector2(spacingOffset.x, 0);
+        }
+        else
+            gridLayoutGroup.spacing = spacingDefault;
+    }
+
+    /*private void ItemsCounter()
+    {
+        spacingItems = elementsContainer.gameObject.transform.childCount;
+        ItemsCounter();
+        Debug.Log(spacingItems);
+
+        if (spacingItems > 5)
+        {
+            //Debug.Log(gridLayoutGroup.spacing.x - 5);
+            Vector2 aaa = new Vector2((gridLayoutGroup.spacing.x - 5), 0);
+
+            gridLayoutGroup.spacing.Set(aaa.x, 0);
+            Debug.Log(aaa.x);
+        }
+    }*/
+}
