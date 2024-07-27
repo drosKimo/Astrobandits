92afe56897fc0cb9a50a3a14f0b7cd07d21f4bee
drosKimo <ilikeuflower@yandex.ru>
2024-07-27 23:47:17 +0300

    Cards update #6
    Появились заготовки под розыгрыш свойства карты

--- Assets/Scripts/Cards/CardProperty.cs
+++ Assets/Scripts/Cards/CardProperty.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using Unity.VisualScripting;
 
 public class CardProperty : MonoBehaviour
 {
@@ -20,8 +19,76 @@ public class CardProperty : MonoBehaviour
 
         switch (getCardItem.nameKey)
         {
+            case "Cards.Name.Bike":
+                playCard.Bike();
+                break;
+
+            case "Cards.Name.Challenge":
+                playCard.Challenge();
+                break;
+
+            case "Cards.Name.Collapsar":
+                playCard.Collapsar();
+                break;
+
+            case "Cards.Name.CryoCharge":
+                playCard.CryoCharge();
+                break;
+
+            case "Cards.Name.CyberImplant":
+                playCard.CyberImplant();
+                break;
+
+            case "Cards.Name.EnergyBlade":
+                playCard.EnergyBlade();
+                break;
+
+            case "Cards.Name.ForceField":
+                playCard.ForceField();
+                break;
+
+            case "Cards.Name.Isabelle":
+                playCard.Isabelle();
+                break;
+
+            case "Cards.Name.Jackpot":
+                playCard.Jackpot();
+                break;
+
+            case "Cards.Name.MutantDealer":
+                playCard.MutantDealer();
+                break;
+
+            case "Cards.Name.PulseRifle":
+                playCard.PulseRifle();
+                break;
+
+            case "Cards.Name.Scorpion":
+                playCard.Scorpion();
+                break;
+
+            case "Cards.Name.Shredder":
+                playCard.Shredder();
+                break;
+
+            case "Cards.Name.Slam":
+                playCard.Slam();
+                break;
+
+            case "Cards.Name.SporeBeer":
+                playCard.SporeBeer();
+                break;
+
+            case "Cards.Name.Turlock":
+                playCard.Turlock();
+                break;
+
+            case "Cards.Name.XenoRunt":
+                playCard.XenoRunt();
+                break;
+
             default:
-                playCard.Bang();
+                Debug.Log("Карта не задана");
                 break;
         }
 
@@ -30,6 +97,39 @@ public class CardProperty : MonoBehaviour
 
     public void PlayBoardCard()
     {
+        playCard = GameObject.FindWithTag("Player").GetComponent<PlayCard>();
+
+        switch (getCardItem.nameKey)
+        {
+            case "Cards.Name.Armageddets":
+                playCard.Armageddets();
+                break;
+
+            case "Cards.Name.Bartender":
+                playCard.Bartender();
+                break;
+
+            case "Cards.Name.Dodge":
+                playCard.Dodge();
+                break;
+
+            case "Cards.Name.Insectoids":
+                playCard.Insectoids();
+                break;
+
+            case "Cards.Name.LootBoxes":
+                playCard.LootBoxes();
+                break;
+
+            case "Cards.Name.Reassembly":
+                playCard.Reassembly();
+                break;
+
+            default:
+                Debug.Log("Карта не задана");
+                break;
+        }
+
         Destroy(gameObject);
         //Debug.Log("Йес минус три");
     }
--- Assets/Scripts/Cards/PlayCard.cs
+++ Assets/Scripts/Cards/PlayCard.cs
@@ -1,10 +1,123 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayCard : MonoBehaviour
 {
-    public void Bang()
+    [SerializeField] SpawnCard spawnCard;
+
+    public void Armageddets()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Bartender()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Bike()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Challenge()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Collapsar()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void CryoCharge()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void CyberImplant()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Dodge()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void EnergyBlade()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void ForceField()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Insectoids()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Isabelle()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Jackpot()
+    {
+        spawnCard.Spawn();
+        spawnCard.Spawn();
+        spawnCard.Spawn();
+    }
+
+    public void LootBoxes()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void MutantDealer()
+    {
+        spawnCard.Spawn();
+        spawnCard.Spawn();
+    }
+
+    public void PulseRifle()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Reassembly()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Scorpion()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Shredder()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Slam()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void SporeBeer()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void Turlock()
+    {
+        Debug.Log("Карта разыграна");
+    }
+
+    public void XenoRunt()
     {
         Debug.Log("Карта разыграна");
     }
