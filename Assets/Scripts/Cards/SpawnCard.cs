6c111fc293f92a97fde7e6fbd77314004cbb2488
drosKimo <ilikeuflower@yandex.ru>
2024-07-28 00:25:12 +0300

    Cards update #7
    Изменен алгоритм именования карт. Теперь не возникает баг, когда имена карт совпадали и анимации начинали прыгать

--- Assets/Scripts/Cards/SpawnCard.cs
+++ Assets/Scripts/Cards/SpawnCard.cs
@@ -1,22 +1,46 @@
-using Akassets.SmoothGridLayout;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnCard : MonoBehaviour
 {
     [SerializeField] GameObject prefab;
     GameObject objParent;
-    LerpToPlaceholder toPlaceholder;
+
     public void Spawn()
     {
-        //Debug.Log("spawn");
         objParent = GameObject.Find("Elements Container");
 
         GameObject newItem;
         newItem = GameObject.Instantiate(prefab);
-        newItem.name = "Item" + $" {objParent.transform.childCount + 1}";
+
+        // большая проверка, чтобы карты не ломались
+        if (objParent.transform.childCount == 0)
+        {
+            newItem.name = "Item" + $" {objParent.transform.childCount + 1}";
+        }
+        else
+        {
+            // начальное имя - имя последней карты в очереди
+            string startName = objParent.transform.GetChild(objParent.transform.childCount - 1).name;
+            string finalName = startName;
+
+            if (objParent.transform.childCount > 1)
+            {
+                // последнее имя - имя предпоследней карты
+                string lastName = objParent.transform.GetChild(objParent.transform.childCount - 2).name;
+
+                int firstIndex = System.Convert.ToInt32(startName.Substring(5));
+                int lastIndex = System.Convert.ToInt32(lastName.Substring(5));
+
+                // если индекс последней карты больше предпоследней, от него идет новый счет карт
+                if (firstIndex > lastIndex)
+                    finalName = startName;
+                else finalName = lastName;
+            }
+            
+            int cardIndex = System.Convert.ToInt32(finalName.Substring(5)) + 1;
+            newItem.name = "Item " + cardIndex;
+        }
+
         newItem.transform.SetParent(objParent.transform, false);
     }
 }
