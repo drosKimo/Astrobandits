24eba8f47ee3c2091d9081e1882b059af26be333
drosKimo <ilikeuflower@yandex.ru>
2024-08-05 10:12:54 +0300

    Scene update
    Сцена изменена. Добавлены дополнительные противники

--- Assets/Scripts/CharacterRole.cs
+++ Assets/Scripts/CharacterRole.cs
@@ -6,4 +6,5 @@ public class CharacterRole : MonoBehaviour
     public Characters character;
     public int currentHP;
     public Roles role;
+    [Tooltip("Номер персонажа в очереди")][Range(0, 6)] public int queueNumber;
 }
