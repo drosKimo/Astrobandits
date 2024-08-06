bf78a0dfb2b38888411f4a08d132bd5d1f2ede47
drosKimo <ilikeuflower@yandex.ru>
2024-08-06 06:15:46 +0300

    Enemy update #2
    Теперь капитан помечается красным цветом
    Теперь капитан будет первым в очереди, чтобы потом было удобнее считать очередность ходов

---
+++ Assets/Scripts/Roles/QueueList.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class QueueList : MonoBehaviour
+{
+    bool capFound = false;
+
+    void Start()
+    {
+        GameObject list = GameObject.Find("Enemies");
+        List<GameObject> GOlist = new List<GameObject>();
+
+        // каждого противника в список
+        for (int i = 0; i < list.transform.childCount; i++)
+        {
+            GOlist.Add(list.transform.GetChild(i).gameObject);
+        }
+        
+        // ищет капитана
+        foreach (GameObject cap in GOlist)
+        {
+            CharacterRole characterRole = cap.GetComponent<CharacterRole>();
+            // временно проверяет, есть ли вообще у игрока роль
+            if (!characterRole.role.IsUnityNull() && characterRole.role.roleName == "Roles.Name.Captain")
+            {
+                capFound = true; // теперь капитан найден
+                cap.GetComponent<SpriteRenderer>().color = Color.red; // меняет цвет кэпа
+            }                
+            else if (!capFound)
+                cap.transform.SetAsLastSibling(); // пока кэп не найден, ставит текущего игрока крайним в очереди
+        }
+    }
+}
